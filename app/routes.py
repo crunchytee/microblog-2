66f19cb3cba34514ee4820c44fa6fb3191a608a9
crunchytee <tosh.robertsbrockway@gmail.com>
2024-09-26 22:11:17 -0700

    Finish chapter 2 - templates, and add some JS for playing around

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,21 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {
+        "username": "Tosh"
+    }
+    posts = [
+        {
+            "author": {"username": "Tosh"},
+            "body": "Beautiful day in Sacramento!"
+        }, 
+        {
+            "author": {"username": "Serhii"},
+            "body": "horrible day in Sacramento..."
+        }
+    ]
+    return render_template("index.html", title="home", user=user, posts=posts)
